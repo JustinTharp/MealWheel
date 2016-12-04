4995cfa2b08765a1789648d53b7b45b91c075496
Austin Shircliff <shircliffa@yahoo.com>
2016-12-03 23:38:29 -0500

    Facebook first steps
    Austin - added code for Facebook. Not working yet

---
+++ app/src/main/java/com/example/justin/mealwheel/FacebookFragment.java
@@ -0,0 +1,90 @@
+package com.example.justin.mealwheel;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import junit.framework.Test;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FacebookFragment extends Fragment {
+
+    private CallbackManager callbackmanager;
+    private TextView welcomeMessage;
+
+
+    private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            if (profile != null)
+            {
+                welcomeMessage.setText("Time To Spin the Wheel" + profile.getFirstName());
+            }
+
+        }
+
+        @Override
+        public void onCancel() {
+
+
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+
+        }
+    };
+
+    public FacebookFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        callbackmanager = CallbackManager.Factory.create();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.facebook_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LoginButton loginButton = (LoginButton) view.findViewById(R.id.facebook_login_button);
+        loginButton.setReadPermissions("publish_actions");
+        loginButton.setFragment(this);
+        loginButton.registerCallback(callbackmanager,mCallback);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackmanager.onActivityResult(requestCode,requestCode,data);
+    }
+}
